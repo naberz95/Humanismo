41504342e16417fafa5a5e649411d31fd880d157
naberz95 <98672442+naberz95@users.noreply.github.com>
2024-10-03 16:29:01 -0500

    ! subida de informacion

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('learn-more').addEventListener('click', function() {
+    alert('Aquí podrías agregar más información interactiva o redirigir a una página más detallada.');
+});
